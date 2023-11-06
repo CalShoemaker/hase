38cf2fd5e4a190309234d5884c76167c1d6bdae2
CalShoemaker <cal.shoemaker@gmail.com>
2023-11-05 23:22:59 -0800

    These will live in a react component lib later

---
+++ packages/react-host/src/components/Widget/Widget.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+
+export const Widget = (props:any) => {
+  return (
+    <Paper sx={{
+      p: 2,
+      display: 'flex',
+      flexDirection: 'column',
+      height: 240,
+    }}>
+    { props.children }
+    </Paper>
+  );
+}
+
+export default Widget
---
+++ packages/react-host/src/libraries/consumables.lib.ts
@@ -0,0 +1,149 @@
+const consumablesLibrary = [{
+  id: 0,
+  name: 'Tears of a Bastard Child.',
+  size: 1,
+  description: "Yes, yes! Taste the tears of unfathomable sadness. So sweet, so yummy.",
+  isConsumable: true,
+  effect: '+10 health',
+  duration: '10s',
+  rarity: 1,
+  quality: 1,
+  cost: -1,
+  value: -1,
+  weight: 0
+},{
+  id: 1,
+  name: '3.5 Sugar',
+  size: 1,
+  description: 'An eighth of some dirty looking sugar',
+  isConsumable: true,
+  effect: '+1 speed, -1 speed',
+  duration: '1s, 2s',
+  rarity: 0.1,
+  quality: 0.1,
+  cost: 5,
+  value: 5,
+  weight: 1
+},{
+  id: 2,
+  name: '1 pack Coolaid',
+  size: 1,
+  description: 'A single pack of Coolaid, used to make Pixie Stix',
+  isConsumable: true,
+  rarity: 0.1,
+  quality: 0.1,
+  cost: 1,
+  value: 1
+},{
+  id: 3,
+  name: '10 pack Coolaid',
+  size: 1,
+  description: 'A 10 pack of Coolaid',
+  isConsumable: true,
+  rarity: 0.1,
+  quality: 0.1,
+  cost: 5,
+  value: 5,
+  inside: [{
+    id: 2,
+    name: '1 pack Coolaid',
+    size: 1,
+    description: 'A single pack of Coolaid, used to make Pixie Stix',
+    isConsumable: true,
+    rarity: 0.1,
+    quality: 0.1,
+    cost: 1,
+    value: 1
+  },{
+    id: 2,
+    name: '1 pack Coolaid',
+    size: 1,
+    description: 'A single pack of Coolaid, used to make Pixie Stix',
+    isConsumable: true,
+    rarity: 0.1,
+    quality: 0.1,
+    cost: 1,
+    value: 1
+  },{
+    id: 2,
+    name: '1 pack Coolaid',
+    size: 1,
+    description: 'A single pack of Coolaid, used to make Pixie Stix',
+    isConsumable: true,
+    rarity: 0.1,
+    quality: 0.1,
+    cost: 1,
+    value: 1
+  },{
+    id: 2,
+    name: '1 pack Coolaid',
+    size: 1,
+    description: 'A single pack of Coolaid, used to make Pixie Stix',
+    isConsumable: true,
+    rarity: 0.1,
+    quality: 0.1,
+    cost: 1,
+    value: 1
+  },{
+    id: 2,
+    name: '1 pack Coolaid',
+    size: 1,
+    description: 'A single pack of Coolaid, used to make Pixie Stix',
+    isConsumable: true,
+    rarity: 0.1,
+    quality: 0.1,
+    cost: 1,
+    value: 1
+  },{
+    id: 2,
+    name: '1 pack Coolaid',
+    size: 1,
+    description: 'A single pack of Coolaid, used to make Pixie Stix',
+    isConsumable: true,
+    rarity: 0.1,
+    quality: 0.1,
+    cost: 1,
+    value: 1
+  },{
+    id: 2,
+    name: '1 pack Coolaid',
+    size: 1,
+    description: 'A single pack of Coolaid, used to make Pixie Stix',
+    isConsumable: true,
+    rarity: 0.1,
+    quality: 0.1,
+    cost: 1,
+    value: 1
+  },{
+    id: 2,
+    name: '1 pack Coolaid',
+    size: 1,
+    description: 'A single pack of Coolaid, used to make Pixie Stix',
+    isConsumable: true,
+    rarity: 0.1,
+    quality: 0.1,
+    cost: 1,
+    value: 1
+  },{
+    id: 2,
+    name: '1 pack Coolaid',
+    size: 1,
+    description: 'A single pack of Coolaid, used to make Pixie Stix',
+    isConsumable: true,
+    rarity: 0.1,
+    quality: 0.1,
+    cost: 1,
+    value: 1
+  },{
+    id: 2,
+    name: '1 pack Coolaid',
+    size: 1,
+    description: 'A single pack of Coolaid, used to make Pixie Stix',
+    isConsumable: true,
+    rarity: 0.1,
+    quality: 0.1,
+    cost: 1,
+    value: 1
+  }]
+}];
+export default consumablesLibrary;
