464c93153da5dc6d3f69ec111de22e875fb28336
CalShoemaker <cal.shoemaker@gmail.com>
2023-11-07 21:13:34 -0800

    Nice home

--- packages/react-host/src/components/page/Home/Home.tsx
+++ packages/react-host/src/components/page/Home/Home.tsx
@@ -1,9 +1,38 @@
+import { Box, Card, CardActions, CardContent, CardMedia, Container, Paper, Typography } from '@mui/material';
 import React, { Suspense } from 'react';
-
+import hase from '../../../images/app/hase.webp';
+import { Link } from '@tanstack/react-router';
 export const Home = () => {
   return (
     <Suspense fallback={<h2>🌀 Loading...</h2>}>
-      <div>Hello from Home!</div>
+      <Container sx={{ py:10, height: '100vh', width:400}} >
+        <Paper sx={{ background:"#fff" }} elevation={5}>
+        <Card>
+          <CardMedia
+            sx={{ height: 240 }}
+          >
+            <img src={ hase } width={200} style={{margin:'50px auto', display:'block'}} />
+          </CardMedia>
+          <CardContent>
+            <Typography gutterBottom variant="h4">
+             Hase Süßkartoffel
+             <Typography variant="h6" >
+              [ˈhaːzə] <span style={{display:'inline-block', width:20}}></span>[zeusːcar:tof':el] 
+            </Typography>
+            </Typography>
+
+
+            <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over 6,000
+              species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Link to='/dogs'>Search Dogs</Link>
+          </CardActions>
+        </Card>
+        </Paper>
+      </Container>
     </Suspense>
   );
 }
