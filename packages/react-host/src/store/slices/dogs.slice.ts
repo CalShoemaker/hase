a2816242ff51dff5f8e000f7969096c5fdbf3184
CalShoemaker <cal.shoemaker@gmail.com>
2023-11-08 17:31:04 -0800

    Strongly typed slice

--- packages/react-host/src/store/slices/dogs.slice.ts
+++ packages/react-host/src/store/slices/dogs.slice.ts
@@ -13,12 +13,38 @@ export interface IDogs {
   dogs: IDog[];
 }
 
+interface IRating {
+  title?: string;
+  stars?: string;
+}
+
+ interface ICharacteristics{
+  content?: Array<string>;
+  rating?: Array<IRating>;
+}
+
+interface IAccordion {
+  title?: string;
+  characteristics?: string;
+}
+
+interface ISpecialCards {
+  rating?: Array<IRating>;
+  accordion?: Array<IAccordion>;
+}
+
+interface ISpecial {
+  cards?: Array<ISpecialCards>
+}
+
 export interface IDog {
   title: string;
   id: string;
   tags?: string[];
   statistics?: object;
   facts?: Array<any>,
+  images?: Array<string>,
+  special?: ISpecial
 }
 
 const dogsInterface:IDog[] = dogs;
@@ -30,6 +56,8 @@ export const dogsSlice = createSlice({
   reducers: {},
 });
 
+// NOTE: Returning root state is anti pattern. Right now store is just a wrapper for an API resource.
+// TODO: Handle console errors.
 export const selectDogs = (state: RootState): IDogs => state;
 export const selectDogById = (ID: string) => (state:RootState):IDog => state.dogs.filter(({ id }) => id === ID)[0];
 export default dogsSlice.reducer;
