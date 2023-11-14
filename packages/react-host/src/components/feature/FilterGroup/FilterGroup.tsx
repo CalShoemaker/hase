e7257f4bb7f4cbad9c9dca40bd8b6be7a25b57c9
CalShoemaker <cal.shoemaker@gmail.com>
2023-11-13 19:22:22 -0800

    Clean up

--- packages/react-host/src/components/feature/FilterGroup/FilterGroup.tsx
+++ packages/react-host/src/components/feature/FilterGroup/FilterGroup.tsx
@@ -1,18 +1,61 @@
-import React from "react";
-import Typography from "@mui/material/Typography";
-import { Box, Slider, ToggleButtonGroup, ToggleButton, ListItem, ListItemIcon, ListItemText } from "@mui/material";
+import React, { Component } from "react";
 import { useDispatch } from "react-redux";
 import { setFilters } from "../../../store/slices/filters.slice";
 import AssessmentIcon from '@mui/icons-material/Assessment';
 import AvTimerIcon from '@mui/icons-material/AvTimer';
 import HeightIcon from '@mui/icons-material/Height';
 import ScaleIcon from '@mui/icons-material/Scale';
+import { Box, Slider, ToggleButtonGroup, ToggleButton, ListItem, ListItemIcon, ListItemText } from "@mui/material";
 
 function valuetext(value: number) {
   return `${value}`;
 }
 
-// TODO: Refactor any type
+function FilterItem(props: any) {
+  const { data, handler, label,  min, max, step, icon } = props.config;
+  return (      
+    <ListItem>
+      <ListItemIcon>
+        { icon }
+      </ListItemIcon>
+      <ListItemText>
+      <Slider
+        getAriaLabel={() => label}
+        value={data}
+        onChange={handler}
+        valueLabelDisplay="auto"
+        getAriaValueText={valuetext}
+        step={step}
+        marks
+        min={min}
+        max={max}
+      />
+      </ListItemText>
+    </ListItem>
+  )
+}
+
+function update(filter: string, payload: string | number |  number[], filters: any) {
+  let { height, weight, life, breed } = filters;
+
+  switch (filter) {
+    case "height":
+      height = payload as number[];
+      break;
+    case "weight":
+      weight = payload as number[];
+      break;
+    case "life":
+      life = payload as number[];
+      break;
+    case "breed":
+      breed = payload as string;
+      break;
+  }
+
+  return { height, weight, life, breed }
+}
+
 export function FilterGroup(props: any) {
   const dispatch = useDispatch();
   const { height, weight, life, breed } = props.filters;
@@ -25,26 +68,35 @@ export function FilterGroup(props: any) {
       event: React.MouseEvent<HTMLElement> | Event,
       newValue: string | number | number[],
     ) => {
-      let { height, weight, life, breed } = props.filters;
-
-      switch (filter) {
-        case "height":
-          height = newValue as number[];
-          break;
-        case "weight":
-          weight = newValue as number[];
-          break;
-        case "life":
-          life = newValue as number[];
-          break;
-        case "breed":
-          breed = newValue as string;
-          break;
-      }
-
-      return !suppress && dispatch<any>(setFilters({ height, weight, breed, life }));
+      return !suppress && dispatch<any>(setFilters(update(filter, newValue, props.filters )));
     };
 
+  const filters = [{
+    data: life,
+    handler: handleChange("life"),
+    label: "Age Range",
+    min: 0,
+    max: 20,
+    step: 1,
+    icon: <AvTimerIcon titleAccess="Age in Years" />
+  },{
+    data: height,
+    handler: handleChange("height"),
+    label: "Height Range",
+    min: 0,
+    max: 40,
+    step: 1,
+    icon: <HeightIcon titleAccess="Height in Inches" />
+  },{
+    data: weight,
+    handler: handleChange("weight"),
+    label: "Weight Range",
+    min: 0,
+    max: 170,
+    step: 5,
+    icon: <ScaleIcon titleAccess="Weight in Pounds" />
+  }]
+    
   return (
     <Box sx={{ backgroundColor: "#f5f5f5" }}>
       <ListItem>
@@ -66,76 +118,13 @@ export function FilterGroup(props: any) {
         </ToggleButtonGroup>
         </ListItemText>
       </ListItem>
-        
-      <ListItem>
-        <ListItemIcon>
-          <AvTimerIcon titleAccess="Age in Years" />
-        </ListItemIcon>
-        <ListItemText>
-          <Slider
-            getAriaLabel={() => "Age Range"}
-            value={life}
-            onChange={handleChange("life")}
-            valueLabelDisplay="auto"
-            getAriaValueText={valuetext}
-            step={1}
-            marks
-            min={0}
-            max={20}
-          />
-          {/* <strong>{ life[0] }-{ life[1] }</strong> */}
-        </ListItemText>
-      </ListItem>
-      <ListItem>
-        <ListItemIcon>
-          <HeightIcon titleAccess="Height in Inches" />
-        </ListItemIcon>
-        <ListItemText>
-            <Slider
-              getAriaLabel={() => "Height Range"}
-              value={height}
-              onChange={handleChange("height")}
-              valueLabelDisplay="auto"
-              getAriaValueText={valuetext}
-              step={1}
-              marks
-              min={0}
-              max={40}
-            />
-            {/* { height[0] }-{ height[1] } */}
-          </ListItemText>
-      </ListItem>
-      <ListItem>
-        <ListItemIcon>
-          <ScaleIcon titleAccess="Weight in Pounds" />
-        </ListItemIcon>
-        <ListItemText>
-        <Slider
-          getAriaLabel={() => "Weight Range"}
-          value={weight}
-          onChange={handleChange("weight")}
-          valueLabelDisplay="auto"
-          getAriaValueText={valuetext}
-          step={5}
-          marks
-          min={0}
-          max={170}
-        />
-        {/* <strong>{ weight[0] }-{ weight[1] }</strong> */}
-        </ListItemText>
-      </ListItem>
 
-      {/* <Box py={1}>
-        <Input
-          id="filter"
-          name="filter"
-          type="text"
-          placeholder="Search"
-          sx={{width:'100%'}}
-          onChange={(event) => props.setFilters(event.target.value)}
-        />
-      </Box> */}
+      { 
+        // NOTE: Data driven for scale
+        filters.map((item, i) => (<FilterItem key={i} config={ item } />)) 
+      }
     </Box>
   );
 }
+
 export default FilterGroup;
--- packages/react-host/src/components/page/Dogs/Dogs.tsx
+++ packages/react-host/src/components/page/Dogs/Dogs.tsx
@@ -1,28 +1,24 @@
 import { Outlet, useParams } from "@tanstack/react-router";
 import React, { Suspense, useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import Dashboard from "../../feature/Dashboard";
+import DogsList from "../DogsList";
+import Sidebar from "../../feature/Sidebar";
 import {
   selectDogs,
   fetchDogs
 } from "../../../store/slices/dogs.slice";
-import { selectFilters, setFilters } from "../../../store/slices/filters.slice";
-import Dashboard from "../../feature/Dashboard";
-import DogsList from "../DogsList";
-import Sidebar from "../../feature/Sidebar";
-import { useDispatch } from "react-redux";
+import { 
+  selectFilters, 
+  setFilters 
+} from "../../../store/slices/filters.slice";
 
-
-
-// TODO: Use this approach for Store -> API 
-// https://redux.js.org/tutorials/essentials/part-8-rtk-query-advanced
 export function Dogs() {
   const { dogId } = useParams({ from: "/Dogs/Dog" });
   const data = useSelector(selectDogs);
   const filters = useSelector(selectFilters);
   const dispatch = useDispatch();
 
-  // NOTE: Requires throttling. Add paginations/infinite scroll, and lazy load by chunk. 
-  // TODO: Cache results, debounce, throttle.
   useEffect(() => {
     dispatch<any>(fetchDogs(filters.filters));
   }, [dispatch, filters]);
